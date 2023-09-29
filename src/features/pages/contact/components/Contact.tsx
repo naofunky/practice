3ccad570a1bf88e0071c1ff61c55176b62490224
Koki Nanao <minsokosoko0107@gmail.com>
2023-09-29 10:14:35 +0900

    feat:追加
    取得データのタイトルをフィルタリングして表示機能追加

--- src/features/pages/contact/components/Contact.tsx
+++ src/features/pages/contact/components/Contact.tsx
@@ -8,22 +8,25 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const Contact = () => {
   // inoutに記述内容
   const [title, setTitle] = useState('');
+  // 単語フィルタリンングしたタイトルを配列で格納
+  const [searchTitle, setSearchTitle] = useState([]);
 
   const changeTitleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
 
-  const contactSearchTitle = () => {
-    const { data, error, isLoading } = useSWR(
-      'https://jsonplaceholder.typicode.com/posts',
-      fetcher
-    );
+  const { data, error, isLoading } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts',
+    fetcher
+  );
 
-    console.log(data, error, isLoading);
+  const contactSearchTitle = () => {
+    const datas = data.filter((item: Post) => item.title.includes(title));
+    setSearchTitle(datas);
   };
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>Error...</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
   return (
     <div>
       <Header />
@@ -41,12 +44,12 @@ const Contact = () => {
             検索
           </button>
         </div>
-        {/* {data.map((item: Post) => (
+        {searchTitle.map((item: Post) => (
           <li key={item.id}>
             <h2>{item.title}</h2>
             <p>{item.body}</p>
           </li>
-        ))} */}
+        ))}
       </div>
     </div>
   );
