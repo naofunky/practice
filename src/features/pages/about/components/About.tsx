715979b6ec75e209c87c536a44bcfb270783ce2e
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-12 10:56:39 +0900

    feat:追加
    パスワード自動発行の機能追加

--- src/features/pages/about/components/About.tsx
+++ src/features/pages/about/components/About.tsx
@@ -1,12 +1,31 @@
 import Header from '../../../Header/components/Header';
 import ToggleBtn from '../../../Button/toggleBtn/components/ToggleBtn';
+import { useState } from 'react';
+import '../../../../App.css';
 
 const About = () => {
+  const [inputPassword, setInputPassword] = useState<string>('');
+
+  function createPassword() {
+    const material =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let password = '';
+    const passwordLength = 16;
+    for (let i = 0; i < passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * material.length);
+      password += material.substring(randomNumber, randomNumber + 1);
+    }
+    console.log(password);
+    setInputPassword(password);
+  }
   return (
     <div>
       <Header />
       <h1>About</h1>
       <ToggleBtn />
+      <h2>パスワード自動生成</h2>
+      <input type="text" className="createPassword" value={inputPassword} />
+      <button onClick={() => createPassword()}>パスワード生成</button>
     </div>
   );
 };
